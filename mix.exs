48350112cbea6e110f7c390facdc4cdad801350c
Keith Thompson <me@keiththomps.com>
2018-04-26 07:57:40 -0400

    Add additional package metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Wargaming.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Wargaming",
+      source_url: "https://github.com/keiththomps/wargaming"
     ]
   end
 
@@ -24,6 +28,20 @@ defmodule Wargaming.MixProject do
       {:httpoison, "~> 1.1"},
       {:poison, "~> 3.1"},
       {:exvcr, "~> 0.10.2", only: [:test, :dev]}
+    ]
+  end
+
+  defp description() do
+    "Client library for WarGaming.net related APIs."
+  end
+
+  defp package() do
+    [
+      name: "wargaming",
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Keith Thompson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/keiththomps/wargaming"}
     ]
   end
 end
